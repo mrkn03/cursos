9d544845c56731d335963391db70de01e0e3209c
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-05 16:43:23 -0300

    Add files via upload

---
+++ Udemy/Python/Aulas/Saida de Dados.py
@@ -0,0 +1,13 @@
+#SAÍDA DE DADOS
+print("Bom dia", end=" ")
+print("Boa noite")
+
+#PLACEHOLDER
+x = "Maria"
+y = 19
+
+print("%s tem %d anos" % (x, y))
+
+x = 10; y = 20
+print(x)
+print(y)
---
+++ Udemy/Python/Aulas/Vetores_Listas.py
@@ -0,0 +1,16 @@
+#DECLARAÇÃO DO VETOR
+
+meu_vetor: [tipo] = [0 for i in range(numero_de_elementos)]
+
+#EXEMPLO
+n = int(input("Quantos números voce vai digitar? "))
+
+vet: [float] = [0 for i in range(n)]
+
+for i in range(0, n):
+    vet[i] = float(input("Digite um número: "))
+print()
+
+print("NUMEROS DIGITADOS:")
+for i in range(0,n):
+    print(vet[i])
