517fc9b1282b4d839ec0e53875f32779c2849952
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-05 15:45:48 -0300

    Add files via upload

---
+++ Udemy/Python/Aulas/Operadores.py
@@ -0,0 +1,41 @@
+#OPERADORES ARITMÉTICOS
+x = 5
+y = 2
+
+z = 5 + 2   #ADIÇAO
+print(z)
+z = 5 - 2   #SUBTRAÇÃO
+print(z)
+z = 5 * 2   #MULTIPLICAÇÃO
+print(z)
+z = 5 / 2   #DIVISÃO
+print(z)
+z = 5 % 2   #RESTO DA DIVISÃO ("MOD")
+print(z)
+z = 5 ** 2  #EXPONENCIAÇÃO
+print(z)
+z = 5 // 2  #DIVISÃO INTEIRA
+print(z)
+
+#OPERADORES COMPARATIVOS
+z = 5 < 2   #5 MENOR QUE 2
+print(z)
+z = 5 > 2   #5 MAIOR QUE 2
+print(z)
+z = 5 <= 2  #5 MENOR OU IGUAL A 2
+print(z)
+z = 5 >= 2  #5 MAIOR OU IGUAL A 2
+print(z)
+z = 5 == 2  #5 IGUAL A
+print(z)
+z = 5 != 2  #5 DIFERENTE DE 2
+print(z)
+
+#OPERADORES LÓGICOS
+z = 5 and 2 #5 E 2
+print(z)
+z = 5 or 2  #5 OU 2 
+print(z)
+#z = 5 not 2 #5 NAO 2
+print(z)
+
---
+++ Udemy/Python/Aulas/Variaveis e Tipos de Dados.py
@@ -0,0 +1,18 @@
+idade: int
+altura: float
+salario: float
+nome: str
+genero: float
+
+idade = 20
+salario = 5800.5
+altura = 1.63
+genero = "F"
+nome = "Maria Silva"
+
+print(f"Idade = {idade}")
+print(f"Salario = {salario}")
+print(f"Altura = {altura}")
+print(f"Genero = {genero}")
+print(f"Nome = {nome}")
+
