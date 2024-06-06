e5a62435c4bfd47f04fccd3dd8b88653bf552bd0
mrkn03 <marcos.fernandes0324@gmail.com>
2024-06-05 21:53:35 -0300

    Update Vetores_Listas.py

--- Udemy/Python/Aulas/Vetores_Listas.py
+++ Udemy/Python/Aulas/Vetores_Listas.py
@@ -1,11 +1,14 @@
 #DECLARAÇÃO DO VETOR
 
-meu_vetor: [tipo] = [0 for i in range(numero_de_elementos)]
+#meu_vetor: [tipo] = [0 for i in range(numero_de_elementos)]
 
 #EXEMPLO
 n = int(input("Quantos números voce vai digitar? "))
 
-vet: [float] = [0 for i in range(n)]
+#vet: [float] = [0 for i in range(n)]
+                #OU
+vet = [] * n
+
 
 for i in range(0, n):
     vet[i] = float(input("Digite um número: "))
