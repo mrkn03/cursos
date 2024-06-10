22caf8017cd36890f6f28d190e64b678dac74301
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-10 16:08:20 -0300

    Delete Curso em Vídeos/Python 3/Exercícios/ex76.py

--- Curso em Vídeos/Python 3/Exercícios/ex76.py
+++
@@ -1,3 +0,0 @@
-"""Crie um programa que tenha uma tupla unica com nomes de produtos e seus respectivos preços na sequencia. No final, mostre uma listagem de preços, organizando
-os dados em forma tabular"""
-
