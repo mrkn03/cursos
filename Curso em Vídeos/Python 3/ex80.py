365dd940f1d59f8cdef3a4a5720e28c9a8fa7bbd
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-12 16:03:33 -0300

    Add files via upload

---
+++ Curso em Vídeos/Python 3/ex80.py
@@ -0,0 +1,2 @@
+"""Crie um programa onde o usuario possa digitar cinco valores numericos e cadastr-os em uma lista, já na posição correta de inserção(sem usar o sort()).
+no final, mostre a lista ordenada na tela"""
