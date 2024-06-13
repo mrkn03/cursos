f885f3aa8c0fd89f55a5f293fc1c4f765f589ff8
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-13 09:10:05 -0300

    Delete Curso em Vídeos/Python 3/ex80.py

--- Curso em Vídeos/Python 3/ex80.py
+++
@@ -1,2 +0,0 @@
-"""Crie um programa onde o usuario possa digitar cinco valores numericos e cadastr-os em uma lista, já na posição correta de inserção(sem usar o sort()).
-no final, mostre a lista ordenada na tela"""
