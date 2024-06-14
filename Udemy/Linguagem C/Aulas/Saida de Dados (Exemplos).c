9ac4b90240f4db00a6b7702615cded1c021218a4
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-14 16:51:02 -0300

    Add files via upload

---
+++ Udemy/Linguagem C/Aulas/Saida de Dados (Exemplos).c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Bom dia\n");
+    printf("Boa noite\n");
+
+
+    return 0;
+}
+
+int main(){
+
+    int x, y;
+
+    x = 10;
+    y = 20;
+
+    printf("%d\n", x);
+    printf("%d\n", y);
+
+    return 0;
+}
+
+int main(){
+
+    double x;
+
+    x = 2.3456;
+
+    printf("%.2lf\n", x);
+
+    return 0;
+}
+
+int main(){
+
+    int idade;
+    double salario;
+    char nome[50];
+    char sexo;
+
+    idade = 32;
+    salario = 4560.9;
+    strcpy(nome, "Maria Silva");
+    sexo = 'F';
+
+    printf("A funcionaria %s, sexo %c, ganha %.2lf e tem %d anos\n", nome, sexo, salario, idade);
+
+
+    return 0;
+}
