af5a0726d9e6cbb236809c7a80901c3710887c63
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-17 16:33:49 -0300

    Add files via upload

---
+++ Udemy/Python/Aulas/Matrizes.py
@@ -0,0 +1,20 @@
+#DECLARAÇÃO DA MATRIZ
+
+#minha_matriz: [[tipo]] = [[0 for x in range(numero_de_colunas)] for x in range(numero_de_linhas)]
+
+#Exemplo
+m = int(input("Quantas linhas vai ter a matriz? "))
+n = int(input("Quantas colunas vai ter a matriz? "))
+
+mat: [[int]] = [[0 for i in range(n)]for j in range (m)]
+
+for i in range(0,m):
+    for j in range(0,n):
+        mat[i][j] = int(input(f"Elemento [{i},{j}]:"))
+    
+print()
+print("MATRIZ DIGITADA: ")
+for i in range(0,m):
+    for j in range(0,n):
+        print(f"{mat[i][j]}", end = "   ")
+    print()
