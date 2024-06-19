a7d16d58e9e98cce73930a8fa38dcb65e8a4411f
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-19 15:16:35 -0300

    Delete Curso em Vídeos/Python 3/Exercícios/ex83.py

--- Curso em Vídeos/Python 3/Exercícios/ex83.py
+++
@@ -1,2 +0,0 @@
-"""Crie um programa onde o usuario digite uma expressao qualquer que use parenteses. Seu aplicativo devera analisar se a expressao passada esta com os parenteses abertos e fechados
-na ordem correta"""
