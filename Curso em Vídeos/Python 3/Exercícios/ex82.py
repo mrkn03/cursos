a6d1186147980b7109e4837e48392e124c9f1fdc
mrkn03 <128204238+mrkn03@users.noreply.github.com>
2024-06-19 15:16:25 -0300

    Delete Curso em Vídeos/Python 3/Exercícios/ex82.py

--- Curso em Vídeos/Python 3/Exercícios/ex82.py
+++
@@ -1,2 +0,0 @@
-"""Crie um programa que vai ler varios números e colocar em uma lista. Depois disso, crie duas listas extras que vao conter apenas os valores pares e os valores impares digitados,
-respectivamente. Ao final, mostre o conteúdo das tres listas geradas."""
