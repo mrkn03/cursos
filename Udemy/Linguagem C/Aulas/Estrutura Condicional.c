3bea6d741581e44ba097405a0a9740f49abec9d3
Marcos Vinicios Fernandes <128204238+mrkn03@users.noreply.github.com>
2024-06-26 15:41:48 -0300

    Add files via upload

---
+++ Udemy/Linguagem C/Aulas/Estrutura Condicional.c
@@ -0,0 +1,37 @@
+#Simples
+if (condicao)
+{
+    comando1
+    comando2
+}
+
+
+#Composta
+if(condicao)
+{
+    comando1
+    comando2
+}
+else
+{
+    comando3
+    comando4
+}
+
+
+#Encadeamento
+if(condicao1)
+{
+    comando1
+    comando2
+}
+else if(condicao2)
+{
+    comando3
+    comando4
+}
+else
+{
+    comando5
+    comando6
+}
