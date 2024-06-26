3483a5104deb324fe52415134d4e0b2d0dd37e57
Marcos Vinicios Fernandes <128204238+mrkn03@users.noreply.github.com>
2024-06-26 11:49:08 -0300

    Delete Curso em Vídeos/Python 3/Exercícios/ex85.py

--- Curso em Vídeos/Python 3/Exercícios/ex85.py
+++
@@ -1,2 +0,0 @@
-"""Crie um programa onde o usuario possa digitar sete valores numericos e cadastre-os em uma lista única que mantenha separados os valores pares e impares.
-No final, mostre os valores pares e ímpares em ordem crescente."""
